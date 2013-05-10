6af370460bf6384623fb4f82acd2317ea7a43a14
yeungeek <yangjian0410@gmail.com>
2013-05-10 13:04:30 +0800

    1. 提交libs 2. 更新.gitignore

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,44 +1,43 @@
-package server;
 
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
-import java.util.logging.Logger;
+package server;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.jboss.netty.util.HashedWheelTimer;
 import org.jboss.netty.util.Timer;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
 public class Server {
-	public static Logger logger = Logger.getLogger("server logger");
+    public static Logger logger = Logger.getLogger("server logger");
 
-	private final int port;
+    private final int port;
 
-	public Server(int port) {
-		this.port = port;
-	}
+    public Server(int port) {
+        this.port = port;
+    }
 
-	Timer timer = new HashedWheelTimer();
+    Timer timer = new HashedWheelTimer();
 
-	public void run() {
-		// Configure the server.
-		ServerBootstrap bootstrap = new ServerBootstrap(
-				new NioServerSocketChannelFactory(
-						Executors.newCachedThreadPool(),
-						Executors.newCachedThreadPool()));
+    public void run() {
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool()));
 
-		// Set up the pipeline factory.
-		bootstrap.setPipelineFactory(new ServerPipelineFactory(timer));
+        // Set up the pipeline factory.
+        bootstrap.setPipelineFactory(new ServerPipelineFactory(timer));
 
-		// Bind and start to accept incoming connections.
-		bootstrap.bind(new InetSocketAddress(port));
-		
-		
-	}
+        // Bind and start to accept incoming connections.
+        bootstrap.bind(new InetSocketAddress(port));
+    }
 
-	public static void main(String[] args) throws Exception {
-		int port = 20134;
-		new Server(port).run();
-		logger.info("server startup success!!!");
-	}
+    public static void main(String[] args) throws Exception {
+        int port = 20134;
+        new Server(port).run();
+        logger.info("server startup success!!!");
+    }
 }
